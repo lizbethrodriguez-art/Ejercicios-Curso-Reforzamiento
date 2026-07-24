44343d96ccc767a6a81812bac1e13ae441c87745
lizbethrodriguez-art <brianalizbethdaviladavilarodriguez@gmail.com>
2026-07-24 17:10:34 -0600

    cambio comentario en area de un rectangulo

--- Area_de_un_rectangulo.py
+++ Area_de_un_rectangulo.py
@@ -3,3 +3,4 @@ base = float(input("Ingrese la base del rectangulo: "))
 altura = float(input("Ingrese la altura del rectangulo: "))
 area = base * altura 
 print("El area del rectangulo es:", area)
+#comentario para mi primer commit 1
